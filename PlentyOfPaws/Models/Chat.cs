1443a61a6f211ee42d0048332b07798900ad39a0
Jared1874 <79114545+Jared1874@users.noreply.github.com>
2022-03-07 14:09:24 +0000

    Add files via upload

---
+++ PlentyOfPaws/Models/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlentyOfPaws.Models
+{
+    public class Chat
+    {
+        // Get the chat ID of the chat
+        public int ChatID { get; set; }
+        // Get the timestamp of the last message that was sent
+        public DateTime ChatTimestamp { get; set; }
+        // Get the UserIDs of the users involved in the chat
+        public int UserIDA { get; set; }
+        public int UserIDB { get; set; }
+    }
+}
---
+++ PlentyOfPaws/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlentyOfPaws.Models
+{
+    public class Message
+    {
+        // Get the content of the message
+        public string Content { get; set; }
+        // Get the user that sent the message
+        public int UserID { get; set; }
+        // Get the timestamp that the message was sent
+        public DateTime MessageTimestamp { get; set; }
+        // Get the chat ID that the message relates to
+        public int ChatID { get; set; }
+    }
+}
