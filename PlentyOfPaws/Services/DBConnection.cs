1fa8793a8de70f7a7050393e134acd2015782c68
ABaldwin02 <79448470+ABaldwin02@users.noreply.github.com>
2022-03-09 17:14:45 +0000

    Add files via upload

---
+++ PlentyOfPaws/Services/DBConnection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace testconnection
+{
+    public class DataBaseConnection
+    {
+        public void run()
+        {
+            string query = "SELECT * FROM tbl_user";
+
+            string passwsd = "";
+
+            //Connection info
+            string MySQLConnectionString = $"server=127.0.0.1;database=db_account_info;user id=root;password={passwsd};";
+
+            //Connects to DB
+            MySqlConnection dbConnect = new MySqlConnection(MySQLConnectionString);
+
+            //Runs query
+            MySqlCommand commanddb = new MySqlCommand(query, dbConnect);
+            commanddb.CommandTimeout = 60;
+
+            try
+            {
+                Console.WriteLine("NOT OPEN");
+                Console.WriteLine(MySQLConnectionString);
+                dbConnect.Open();
+                Console.WriteLine("OPEN");
+
+                MySqlDataReader reader = commanddb.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        //1 GetString() per row
+                        Console.WriteLine(reader.GetString(0) + " - " + reader.GetString(1) + " - " + reader.GetString(2) + " - " + reader.GetString(3) + " - " + reader.GetString(4));
+                        Console.WriteLine("Worked");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Error msg
+                Console.WriteLine("Something went wrong\n" + e.Message);
+            }
+        }
+    }
+}
