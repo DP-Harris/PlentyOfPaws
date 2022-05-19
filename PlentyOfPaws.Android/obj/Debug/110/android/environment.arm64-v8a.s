f2fd40d3b531bd5f9021b53da0b2fbb297b1ee42
Dan <30038625@student.northlindsey.ac.uk>
2022-05-19 15:31:15 +0100

    Byte array occupied but not present in data exchange

--- PlentyOfPaws.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ PlentyOfPaws.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -85,7 +85,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"181080be-0dad-49d0-81ca-0b56a58a1539"
+	.asciz	"5011c96f-259e-4605-ab3c-6e821ff7a4e4"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- PlentyOfPaws/Models/IFilePicker.cs
+++ PlentyOfPaws/Models/IFilePicker.cs
@@ -15,19 +15,14 @@ namespace PlentyOfPaws.Models
         public static async void PickAndShow(PickOptions options)
         {
             try
-            {
-                // Define byte array named Blob
-                byte[] Blob = new byte[0];
+            { 
                 var result = await FilePicker.PickAsync(options);
                 if (result != null)
-                {
-                    var Text = $"File Name: {result.FileName}";
-                    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
-                    result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                {             
                     {
                         var stream = await result.OpenReadAsync();
-                        Console.WriteLine(stream);
-                        var image = ImageSource.FromStream(() => stream);
+                       // Console.WriteLine(stream);
+                       // var image = ImageSource.FromStream(() => stream);
 
 
 
@@ -36,18 +31,14 @@ namespace PlentyOfPaws.Models
                         using (MemoryStream ms = new MemoryStream())
                         {
                             stream.CopyTo(ms);
-                            Blob = ms.ToArray();
+                            bytearray = ms.ToArray();
+                       
+
                             ms.Dispose();
                         }
-                        // Return the byte array
-                        bytearray = Blob;
+
                     }
                 }
-
-
-
-                // Return the byte array
-                bytearray = Blob;
             }
             catch (Exception ex)
             {
